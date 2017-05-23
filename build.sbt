147f2d378cbd8c48490db993402d31c6edfa8239
Ben Hutchison <ben.hutchison@rea-group.com>
2017-05-23 10:29:33 +1000

    Demo effect on batch size on concurrency

---
+++ build.sbt
@@ -0,0 +1,9 @@
+libraryDependencies ++= Seq(
+	"org.atnos" %% "eff-monix" % "4.2.0"
+)
+
+scalaVersion := "2.12.2"
+
+name := "TaskConcurrencyExperiment"
+
+
---
+++ src/main/scala/concur/Main.scala
@@ -0,0 +1,33 @@
+package concur
+
+object Main extends App {
+  import org.atnos.eff._
+  import org.atnos.eff.all._
+  import org.atnos.eff.syntax.all._
+  import monix.eval.Task
+  import org.atnos.eff.addon.monix.task._
+  import org.atnos.eff.syntax.addon.monix.task._
+  import cats.implicits._
+  import monix.eval._
+  import monix.execution._
+  import monix.execution.schedulers._
+
+  val n = 20
+  val pool = java.util.concurrent.Executors.newScheduledThreadPool(n)
+//  implicit val s = monix.execution.Scheduler(pool, ExecutionModel.BatchedExecution(4))
+  implicit val s = monix.execution.Scheduler(pool, ExecutionModel.BatchedExecution(16))
+
+  type S = Fx.fx1[Task]
+
+  val latch = new java.util.concurrent.CountDownLatch(n)
+
+  def countdown = taskSuspend(Task.eval({println("countDown"); latch.countDown(); latch.await()}.pureEff[S]))
+
+  println("Begin...")
+  val task = Eff.traverseA((1 to 10).toList)(_ => countdown)
+  task.runAsync.runAsync
+  println("Await...")
+  latch.await()
+  println("Talk less, listen more")
+  pool.shutdown()
+}
